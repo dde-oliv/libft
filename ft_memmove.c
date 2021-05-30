3365aea6d9e172273fbc3ec573eeaeafcacd91b9
dde-oliv <doliveira@doliveira.localdomain>
2021-05-29 23:52:46 -0300

    changes on memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: dde-oliv <dde-oliv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/15 10:46:43 by dde-oliv          #+#    #+#             */
-/*   Updated: 2021/05/19 12:17:46 by dde-oliv         ###   ########.fr       */
+/*   Updated: 2021/05/29 23:49:11 by dde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,15 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	char	*temp;
-	size_t	count;
-
-	count = 0;
-	temp = ft_calloc(n, sizeof(char));
-	while (count < n && src + count != dest + count)
+	if (dest > src)
 	{
-		temp[count] = *(char *)(src + count);
-		count++;
+		while (n > 0)
+		{
+			*(unsigned char *)(dest + n - 1) = *(unsigned char *)(src + n - 1);
+			n--;
+		}
 	}
-	ft_memcpy(dest, temp, n);
+	else
+		ft_memcpy(dest, src, n);
 	return (dest);
 }
