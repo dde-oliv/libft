35c4c8da40e6ae17a032f6ce31234f6579c40ecf
dde-oliv <doliveira@doliveira.localdomain>
2021-06-01 13:24:48 -0300

    added a protection

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: dde-oliv <dde-oliv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 10:29:38 by dde-oliv          #+#    #+#             */
-/*   Updated: 2021/05/19 12:17:03 by dde-oliv         ###   ########.fr       */
+/*   Updated: 2021/06/01 13:23:01 by dde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	ft_putstr_fd(char *s, int fd)
 	int	slen;
 	int	count;
 
+	if (!s)
+		return ;
 	count = 0;
 	slen = ft_strlen(s);
 	while (count < slen)
