f43a55fbe6deeddee83e98c8cb14e8d3605d4686
dde-oliv <doliveira@doliveira.localdomain>
2021-06-01 13:56:55 -0300

    added protection

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: dde-oliv <dde-oliv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/14 17:43:46 by dde-oliv          #+#    #+#             */
-/*   Updated: 2021/05/29 19:49:45 by dde-oliv         ###   ########.fr       */
+/*   Updated: 2021/06/01 13:55:12 by dde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	count;
 
+	if (!dest && !src)
+		return (dest);
 	count = 0;
 	while (count < n)
 	{
