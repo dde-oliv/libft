3be095365294ef10c705413c8ac97c15c17ea557
dde-oliv <doliveira@doliveira.localdomain>
2021-06-01 21:05:13 -0300

    some new features, adequating ft_atoi to overflows and underflows

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,21 +6,33 @@
 /*   By: dde-oliv <dde-oliv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/16 11:00:02 by dde-oliv          #+#    #+#             */
-/*   Updated: 2021/05/16 11:32:29 by dde-oliv         ###   ########.fr       */
+/*   Updated: 2021/06/01 21:01:20 by dde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_overflow(long int num, const char *str)
+{
+	if (0 < num && (LONG_MAX / 10 < num
+			|| (LONG_MAX / 10 == num && LONG_MAX % 10 < *str - 48)))
+		return (1);
+	else if (0 > num && (LONG_MIN / 10 > num
+			|| (LONG_MIN / 10 == num && -(LONG_MIN % -10) < *str - 48)))
+		return (-1);
+	else
+		return (0);
+}
+
 int	ft_atoi(const char *str)
 {
-	int	sig;
-	int	num;
+	int			sig;
+	int			is_overflow;
+	long int	num;
 
 	sig = 1;
 	num = 0;
-	while (*str == ' ' || *str == '\n' || *str == '\r'
-		|| *str == '\t' || *str == '\v' || *str == '\f')
+	while (ft_isspace(*str))
 		str++;
 	if (*str == '-' || *str == '+')
 	{	
@@ -30,8 +42,13 @@ int	ft_atoi(const char *str)
 	}
 	while (47 < *str && *str < 58)
 	{
-		num = 10 * num + (*str - 48);
+		is_overflow = ft_overflow(num * sig, str);
+		if (is_overflow == 1)
+			return ((int) LONG_MAX);
+		else if (is_overflow == -1)
+			return ((int) LONG_MIN);
+		num = (10 * num + (*str - 48));
 		str++;
 	}
-	return (sig * num);
+	return ((int)(sig * num));
 }
