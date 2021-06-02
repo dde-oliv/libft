e78d08fd7a77360aeaa6a6710df2ad5ddb27e34b
dde-oliv <doliveira@doliveira.localdomain>
2021-06-01 22:01:53 -0300

    few changes, so the function does not use more memory than it's needed

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: dde-oliv <dde-oliv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 11:39:01 by dde-oliv          #+#    #+#             */
-/*   Updated: 2021/05/18 12:35:14 by dde-oliv         ###   ########.fr       */
+/*   Updated: 2021/06/01 21:59:49 by dde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,19 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char			*substr;
 	unsigned int	len_s;
 	unsigned int	count;
+	unsigned int	size;
 
 	if (!s)
 		return (NULL);
 	len_s = ft_strlen((char *)s);
-	substr = malloc(sizeof(char) * (len + 1));
+	size = 0;
+	while (start < len_s && size < len && s[start + size] != '\0')
+		size++;
+	substr = malloc(sizeof(char) * (size + 1));
 	if (!substr)
 		return (NULL);
 	count = 0;
-	while (start < len_s && count < len && s[start + count] != '\0')
+	while (count < size)
 	{
 		substr[count] = s[start + count];
 		count++;
